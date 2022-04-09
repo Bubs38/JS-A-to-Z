d7fd2523ab2c17a393b21261285ee6478b5b8ba1
Bubs38 <burgain.aurelien@gmail.com>
2022-04-09 19:24:34 +0200

    Version 16

---
+++ JS-P3/form-checker/index.js
@@ -0,0 +1,97 @@
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+
+const progressBar = document.getElementById("progress-bar");
+let pseudo, email, password, confirmpass;
+
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+const pseudoChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères.");
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractère spéciaux."
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+  }
+};
+
+const emailChecker = (value) => {
+  if (
+    !value.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    errorDisplay("email", "Le mail n'est pas valide.");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+  }
+};
+
+const passwordChecker = (value) => {
+  progressBar.classList = "";
+  if (
+    !value.match(
+      /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+    )
+  ) {
+    errorDisplay(
+      "password",
+      "minimum de 8 caractères, une majuscule, un chiffre et caractère spécial."
+    );
+    progressBar.classList.add("progressRed");
+    password = null;
+  } else if (value.length < 12) {
+    progressBar.classList.add("progressBlue");
+    errorDisplay("password", "", true);
+    password = value;
+  } else {
+    progressBar.classList.add("progressGreen");
+    errorDisplay("password", "", true);
+    password = value;
+  }
+};
+
+const confirmChecker = (value) => {
+  console.log(value);
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "peudo":
+        pseudoChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
