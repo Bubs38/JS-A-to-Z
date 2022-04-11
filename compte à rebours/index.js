05055536bc042592c9555c09dc51a41949584490
Bubs38 <burgain.aurelien@gmail.com>
2022-04-11 23:34:47 +0200

    Version 20

---
+++ compte à rebours/index.js
@@ -0,0 +1,31 @@
+
+let totalSeconds;
+let interval;
+
+const countDown = () => {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  let sec = seconds < 10 ? "0" + seconds : seconds;
+
+  countdownDisplay.textContent = `${minutes} : ${sec}`;
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.textContent = "c'est terminé !";
+    clearInterval(interval);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (isNaN(choice.value)) {
+    alert("Entrer un nombre !");
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = "";
+    clearInterval(interval);
+    interval = setInterval(countDown, 1000);
+  }
+});
+
