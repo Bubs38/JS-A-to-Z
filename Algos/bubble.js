fbb38937a0ac694d7fd61a5f12054776965d2849
Bubs38 <burgain.aurelien@gmail.com>
2022-04-13 22:47:26 +0200

    Version 22

---
+++ Algos/bubble.js
@@ -0,0 +1,22 @@
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length -1; i++) {
+    for (let j = 0; j < array.length -1 -i; j++) {
+      if (array[j] > array[j+1]) {
+        // let temp = array[j];
+        // array[j] = array[j+1];
+        // array[j+1] = temp;
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+      }
+    } 
+  }
+  console.log(array);
+}
+
+bubbleSort([451, 2, 65, 4, 7, 98, 2, 2, 1])
+
+// faisable plus simplement avec la methode sort
+
+const bubble = (array) => {
+  console.log(array.sort((a, b) => a - b));
+}
+bubble([451, 2, 65, 4, 7, 98, 2, 2, 1])
